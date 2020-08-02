1faab24ab993585a0f27633670a55200dee43815
Sam Leon <Sale3054@colorado.edu>
2020-08-02 17:19:47 -0600

    checkpoint

--- read-in-seeds.c
+++ read-in-seeds.c
@@ -4,26 +4,47 @@
 #include "cubiomes/finders.h"
 #include "cubiomes/generator.h"
 
-#define LSIZ 25
-#define RSIZ 123835
+#define SEED_LEN 25
+#define NUM_SEEDS 123835
 
-int main(void)
+int main()
 {
+  initBiomes();
 
-  #define LSIZ 25
-  #define RSIZ 123835
+  LayerStack g = setupGenerator(MC_1_14);
 
-  char line[RSIZ][LSIZ];
+  char line[NUM_SEEDS][SEED_LEN];
   char fname[25] = "seeds-filtered.txt";
   FILE *fptr = NULL;
   int i = 0;
 
   fptr = fopen(fname, "r");
   printf("Opened the file...\n");
-  while(fgets(line[i], LSIZ, fptr))
+  while(fgets(line[i], SEED_LEN, fptr))
 	{
     printf("%s", line[i]);
     i++;
   }
   return 0;
+}
+
+char** getSeedList()
+{
+  char line[NUM_SEEDS][SEED_LEN];
+  char ** seedList = malloc(NUM_SEEDS * sizeof(char*));
+  char fname[25] = "seeds-filtered.txt";
+  FILE *fptr = NULL;
+
+  fptr = fopen(fname, "r");
+  printf("Opened the file...\n");
+
+  int = 0;
+  while (fgets(line[i], SEED_LEN, fptr)
+  {
+    seedList[i] = malloc(SEED_LEN * sizeof(char) + 1);
+    i++;
+  }
+
+  return seedList;
+
 }
