e364beaa45b9bb4aa1933e3984b08d5cb3d047d7
Sam Leon <Sale3054@colorado.edu>
2020-08-11 21:54:18 -0600

    this sort of works...

--- read-in-seeds.c
+++ read-in-seeds.c
@@ -22,31 +22,43 @@ char** getSeedList()
   {
     seedList[i] = malloc(SEED_LEN * sizeof(char) + 1);
     seedList[i] = line[i];
-    printf(line[i]);
     i++;
   }
   seedList[i] = NULL;
   return seedList;
 }
 
+
+
 int main()
 {
   initBiomes();
   LayerStack g = setupGenerator(MC_1_16);
   char ** str = getSeedList();
+  int biomeList[3] = {warmOcean, ice_spikes, mushroom_fields};
+  BiomeFilter myBiomeFilter = setupBiomeFilter(biomeList, 3);
+  int * map = NULL;
+  printf("Finished getting seeds from cleaned seedList/File/W.e\n");
+  printf("%d", warmOcean);
 
   // Pos pos = getSpawn(const int mcversion, const LayerStack *g, int *cache, int64_t worldSeed)
   for (int i = 0; i < NUM_SEEDS - 100; i++)
-  {1
+  {
+    long long winningSeed = 0;
     if(str[i] != NULL)
     {
       int64_t seedNum = atoi(str[i]);
+      printf("%lld", seedNum);
       Pos pos = getSpawn(MC_1_16, &g, NULL, seedNum);
       applySeed(&g, seedNum);
-      int biomeID = getBiomeAtPos(g, pos);
-      if (biomeID == jungle_edge)
+      winningSeed = checkForBiomes(&g, map, seedNum, pos.x, pos.z, 1000, 1000, myBiomeFilter, 27);
+      if(winningSeed == 0)
       {
-        break;
+        printf("Seed #: %"PRId64" did not have all we were looking for...\n", seedNum);
+      }
+      else
+      {
+        printf("Success!\n Seed #: %"PRId64" has all we are looking for!!\n", seedNum);
       }
     }
   }
